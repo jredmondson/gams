f1e93b01fe33369281bc060b64ca5ed918a4ee22
Anton Dukeman <anton.dukeman@gmail.com>
2015-06-08 13:51:52 -0400

    Summit: * fixed initialization in V-REP * increased default speed for Summit model in V-REP

--- src/gams/platforms/Controller_Platform_Factory.cpp
+++ src/gams/platforms/Controller_Platform_Factory.cpp
@@ -57,6 +57,7 @@
 #ifdef _GAMS_VREP_
 #include "gams/platforms/vrep/VREP_UAV.h"
 #include "gams/platforms/vrep/VREP_Ant.h"
+#include "gams/platforms/vrep/VREP_Summit.h"
 #endif
 
 #include <string>
@@ -110,12 +111,18 @@ platforms::Controller_Platform_Factory::initialize_default_mappings (void)
   add (aliases, new VREP_Ant_Factory ());
   
   // the VREP Ant platform
-  aliases.resize (3);
+  aliases.resize (2);
   aliases[0] = "vrep-uav";
   aliases[1] = "vrep_uav";
-  aliases[2] = "vrep";
 
   add (aliases, new VREP_UAV_Factory ());
+
+  // the VREP Summit platform
+  aliases.resize (2);
+  aliases[0] = "vrep-summit";
+  aliases[1] = "vrep_summit";
+
+  add (aliases, new VREP_Summit_Factory ());
 #endif
 
 
