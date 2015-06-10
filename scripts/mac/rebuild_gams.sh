4eacf848750ec1dd0df8e7089bf2228a39f015aa
Anton Dukeman <anton.dukeman@gmail.com>
2015-06-10 17:24:21 -0400

    Mac Scripts: * copied Linux rebuild_gams script and modified for Mac

---
+++ scripts/mac/rebuild_gams.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# rebuild GAMS...useful when swapping branches
+
+cd $GAMS_ROOT
+make realclean -j $CORES
+perl $ACE_ROOT/bin/mwc.pl -type make gams.mwc
+make realclean -j $CORES
+make vrep=1 -j $CORES
