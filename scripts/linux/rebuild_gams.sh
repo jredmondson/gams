16d0de4057530c7ec41e74100988620c2ba72aa2
Anton Dukeman <anton.dukeman@gmail.com>
2015-06-10 17:16:56 -0400

    Linux Script: * added script to rebuild GAMS, useful for swapping branches

---
+++ scripts/linux/rebuild_gams.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# rebuild GAMS...useful when swapping branches
+
+cd $GAMS_ROOT
+make realclean -j $CORES
+perl $ACE_ROOT/bin/mwc.pl -type gnuace gams.mwc
+make realclean -j $CORES
+make vrep=$VREP $CORES
