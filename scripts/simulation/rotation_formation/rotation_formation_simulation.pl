2fb6f03db2943cd5f99cd4ce62edcc705bc3ac6a
Anton Dukeman <anton.dukeman@gmail.com>
2015-09-20 14:05:03 -0500

    Formation: * updated for Double_Vector instead of string parsing

--- scripts/simulation/rotation_formation/rotation_formation_simulation.pl
+++ scripts/simulation/rotation_formation/rotation_formation_simulation.pl
@@ -2,13 +2,15 @@
 use lib "$ENV{GAMS_ROOT}/scripts/simulation";
 use simulation;
 
-$gams_root = $ENV{GAMS_ROOT};
-$time = 3600;
-$madara_debug = 0;
-$gams_debug = 0;
-$period = 0.5;
 $num = 5;
+$time = 3600;
+$period = 0.5;
 $sim = "rotation_formation";
 $area = "small";
+$madara_debug = 0;
+$gams_debug = 6;
+$border = "";
+$num_coverages = 0;
+$launch_controllers = 1;
 
-simulation::run($num, $time, $period, $sim, $area, $madara_debug, $gams_debug);
+simulation::run($num, $time, $period, $sim, $area, $madara_debug, $gams_debug, $border, $num_coverages, $launch_controllers);
