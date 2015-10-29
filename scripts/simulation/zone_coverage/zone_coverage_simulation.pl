5bad939e35875c3beaa11ca1c01aa95c6aecbc67
David Kyle <dskyle@sei.cmu.edu>
2015-10-29 15:17:03 -0400

    Added zone coverage test simulation

---
+++ scripts/simulation/zone_coverage/zone_coverage_simulation.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+use lib "$ENV{GAMS_ROOT}/scripts/simulation";
+use simulation;
+
+$gams_root = $ENV{GAMS_ROOT};
+$time = 3600;
+$madara_debug = 0;
+$gams_debug = 5;
+$period = 1;
+$num = 6;
+$sim = "zone_coverage";
+$area = "small";
+$border = "region.0";
+$num_coverages = 0;
+$launch_controllers = 1;
+
+simulation::run($num, $time, $period, $sim, $area, $madara_debug, $gams_debug, $border, $num_coverages, $launch_controllers);
