2f01fa41aca057473c4091f2ff45729ccb148f35
James Edmondson <jedmondson@gmail.com>
2016-06-23 09:17:42 -0400

    * SCRIPTS: modified build_c++.bat to allow for downloading the script and base_build.bat alone and running them without GAMS being downloaded first

--- scripts/windows/build_c++.bat
+++ scripts/windows/build_c++.bat
@@ -1,1 +1,5 @@
-call %GAMS_ROOT%\scripts\windows\base_build.bat ace madara gams vrep vrep-config %*
+
+SET SCRIPTS_DIR=%~dp0
+SET SCRIPTS_DIR=%SCRIPTS_DIR:~0,-1%
+
+call %SCRIPTS_DIR%\base_build.bat ace madara gams vrep vrep-config %*
