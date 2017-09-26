31cf044b632edfaf54b8da3c2e4a8fff511c2cfb
David Kyle <dskyle@sei.cmu.edu>
2017-09-26 13:48:32 -0400

    Add feature tests to CPP11 compatibility checking

--- src/gams/CPP11_compat.h
+++ src/gams/CPP11_compat.h
@@ -7,24 +7,28 @@
 #define WARN_UNUSED
 #endif
 
-#if __cplusplus >= 201103L || (_MSC_VER > 1800) 
+#if __cplusplus >= 201103L || _MSC_VER >= 1900
 // If C++11 support is active
 #define CPP11
 #else
 // If C++11 support is not active
 
+#if __cpp_constexpr >= 200704
+#else
 // constexpr simply ignored if not supported
 #define constexpr
+#endif
 
 // override simply ignored if not supported
 #define override
 
+#if __cpp_static_assert >= 200410L
+#else
 // approximate static_assert as best as possible
 #define static_assert(pred, message) \
      /* error: static_assert failure, see reason below */ typedef int sa_fail__\
 [(pred) ? 1 : -1]
-
-#define nullptr NULL
+#endif
 
 #endif
 
