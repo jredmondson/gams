001e47fb48370955588f1120245fc93754575076
Devon Ash <noobaca2@gmail.com>
2019-01-26 02:16:48 -0500

    Added in using_airlib and base of airlib into platforms

---
+++ src/gams/platforms/airlib/AirLibBase.cpp
@@ -0,0 +1,100 @@
+/**
+ * @file AirLibBase.cpp
+ * @author Devon Ash <noobaca2@gmail.com>
+ *
+ * This file contains the definition of the AirLibBase abstract class
+ **/
+
+#ifdef _GAMS_AIRLIB_ // only compile this if we are simulating in Unreal's AirSim
+
+#include "gams/platforms/airlib/AirLibBase.h"
+
+#include <iostream>
+#include <cmath>
+#include <map>
+#include <vector>
+
+#include "madara/knowledge/containers/DoubleVector.h"
+
+using std::endl;
+using std::cout;
+using std::cerr;
+using std::string;
+using std::map;
+
+gams::platforms::AirLibBase::AirLibBase (madara::knowledge::KnowledgeBase * knowledge,
+  variables::Sensors * sensors, variables::Self * self) :
+  BasePlatform (knowledge, sensors, self), ready_ (false)
+{
+  static bool init = false;
+
+  if (!init)
+  {
+//    string node_name (knowledge->get (".ros_node").to_string ());
+//    map<string, string> remap;
+//    ros::init (remap, node_name);
+    init = true;
+  }
+}
+
+int
+gams::platforms::AirLibBase::sense ()
+{
+  return 1;
+}
+
+int
+gams::platforms::AirLibBase::analyze ()
+{
+  return 1;
+}
+
+double
+gams::platforms::AirLibBase::get_accuracy () const
+{
+  return 1;
+}
+
+int
+gams::platforms::AirLibBase::land ()
+{
+  return 1;
+}
+
+int
+land ()
+{
+  return 1;
+}
+
+int
+gams::platforms::AirLibBase::move (const pose::Position & /*position*/,
+        const PositionBounds & /*bounds*/)
+{
+  return 1;
+}
+
+void
+gams::platforms::AirLibBase::set_move_speed (const double & /*speed*/)
+{
+}
+
+int
+gams::platforms::AirLibBase::takeoff ()
+{
+  return 1;
+}
+
+void
+gams::platforms::AirLibBase::wait_for_go () const
+{
+}
+
+const gams::pose::ReferenceFrame &
+gams::platforms::AirLibBase::get_frame (void) const
+{
+  return pose::gps_frame();
+}
+
+#endif // _GAMS_AIRLIB_
+
---
+++ src/gams/platforms/airlib/AirLibBase.h
@@ -0,0 +1,123 @@
+/**
+ * @file AirLibBase.h
+ * @author Devon Ash <noobaca2@gmail.com>
+ *
+ * This file contains the definition of the AirLibBase abstract class
+ **/
+
+#ifndef   _GAMS_PLATFORM_AIRLIB_BASE_H_
+#define   _GAMS_PLATFORM_AIRLIB_BASE_H_
+
+#ifdef _GAMS_AIRLIB_
+
+#include <vehicles/multirotor/api/MultirotorRpcLibClient.hpp>
+
+#include "gams/variables/Self.h"
+#include "gams/variables/Sensor.h"
+#include "gams/variables/PlatformStatus.h"
+#include "gams/platforms/BasePlatform.h"
+#include "gams/pose/GPSFrame.h"
+#include "gams/pose/CartesianFrame.h"
+#include "madara/knowledge/KnowledgeBase.h"
+
+#ifdef __GNUC__
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wpedantic"
+#endif
+
+#ifdef __GNUC__
+#pragma GCC diagnostic pop
+#endif
+
+namespace gams
+{
+  namespace platforms
+  {
+    class GAMS_EXPORT AirLibBase : public BasePlatform
+    {
+    public:
+      /**
+       * Constructor
+       * @param  knowledge  knowledge base
+       * @param  sensors    map of sensor names to sensor information
+       * @param  platforms  map of platform names to platform information
+       * @param  self       device variables that describe self state
+       **/
+      AirLibBase (
+        madara::knowledge::KnowledgeBase * knowledge,
+        variables::Sensors * sensors,
+        variables::Self * self);
+
+      /**
+       * Polls the sensor environment for useful information
+       * @return number of sensors updated/used
+       **/
+      virtual int sense (void) override;
+
+      /**
+       * Analyzes platform information
+       * @return bitmask status of the platform. @see Status.
+       **/
+      virtual int analyze (void) override;
+
+      /**
+       * Get the position accuracy in meters
+       * @return position accuracy
+       **/
+      virtual double get_accuracy () const override;
+
+      /**
+       * Instructs the platform to land
+       * @return 1 if moving, 2 if arrived, 0 if error
+       **/
+      virtual int land (void) override;
+
+      /**
+       * Moves the platform to a position
+       * @param   position  the coordinate to move to
+       * @param   epsilon   approximation value
+       * @return 1 if moving, 2 if arrived, 0 if error
+       **/
+      int move (const pose::Position & position,
+        const PositionBounds &bounds) override;
+
+      // inherit BasePlatform's move overloads
+      using BasePlatform::move;
+      
+      /**
+       * Set move speed
+       * @param speed new speed in meters/loop execution
+       **/
+      virtual void set_move_speed (const double& speed) override;
+
+      /**
+       * Instructs the platform to take off
+       * @return 1 if moving, 2 if arrived, 0 if error
+       **/
+      virtual int takeoff (void) override;
+
+      /**
+       * Returns the reference frame for the platform (usually GPS)
+       * @return the platform's reference frame for positions
+       **/
+      virtual const pose::ReferenceFrame & get_frame (void) const override;
+
+
+    protected:
+      /// the current frame (can theoretically be switched between options)
+      pose::ReferenceFrame * frame_;
+
+      /**
+       * wait for go signal from controller
+       */
+      void wait_for_go () const;
+
+      /// flag for simulated robot ready to receive instruction
+      bool ready_;
+    }; // class AirLibBase
+  } // namespace platform
+} // namespace gams
+
+#endif // _GAMS_AIRLIB_
+
+#endif // _GAMS_PLATFORM_AIRLIB_BASE_H_
