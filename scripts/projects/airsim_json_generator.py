c2a43057ab1b47bee4bf2f14e3fa49ceae9bea89
Devon Ash <noobaca2@gmail.com>
2019-02-05 17:45:53 -0500

    Remove old code

--- scripts/projects/airsim_json_generator.py
+++ scripts/projects/airsim_json_generator.py
@@ -1,12 +1,6 @@
 #!/bin/python
 import sys
 
-def print_madara(x):
-    for i in range(0, int(x)):
-        template = """ group.nodes.members.%d="agent.%d"; """ % (i, i)
-        print template
-    return
-
 def print_json(x):
     print """ {
   "SettingsVersion": 1.2,
@@ -27,7 +21,5 @@ def print_json(x):
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         print "Usage: python settings_json.py madara|json num_agents"
-    elif sys.argv[1] == "madara":
-        print_madara(sys.argv[2])
     elif sys.argv[1] == "json":
         print_json(sys.argv[2])
