05f1ddfddeca19539181af9299eab86fda5161df
Devon Ash <noobaca2@gmail.com>
2020-03-10 21:45:36 -0400

    Add default variables for car controller plugin?

--- src/gams/platforms/scrimmage/GAMSAutonomy.cpp
+++ src/gams/platforms/scrimmage/GAMSAutonomy.cpp
@@ -7,6 +7,9 @@
 // GAMS INCLUDES
 #include <gams/platforms/scrimmage/GAMSAutonomy.h>
 
+// CAR CONTROLLER PLUGIN INCLUDES
+#include <scrimmage/common/VariableIO.h>
+
 REGISTER_PLUGIN(scrimmage::Autonomy, scrimmage::autonomy::GAMSAutonomy, GAMSAutonomy_plugin)
 
 namespace scrimmage {
@@ -20,6 +23,9 @@ GAMSAutonomy::GAMSAutonomy() {
 
 */
 void GAMSAutonomy::init(std::map<std::string, std::string> &params) {
+    desired_heading_idx_ = vars_.declare("desired_heading", scrimmage::VariableIO::Direction::Out);
+    desired_speed_idx_ = vars_.declare("desired_speed", scrimmage::VariableIO::Direction::Out);
+
 
 }
 
@@ -29,6 +35,8 @@ void GAMSAutonomy::init(std::map<std::string, std::string> &params) {
    @effect Changes the entities current state and desired state
 */
 bool GAMSAutonomy::step_autonomy(double t, double dt) {
+   vars_.output(desired_heading_idx_, value_to_output);
+   vars_.output(desired_speed_idx_, value_to_output);
 
 
    return true;
